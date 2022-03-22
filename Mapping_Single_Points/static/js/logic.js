d338ecfed428f8fe0e5b8398de87158f91a89747
swathy84 <93801581+swathy84@users.noreply.github.com>
2022-03-22 14:34:36 -0500

    Adding updated files to Mapping_Single_Points folder

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,20 +2,45 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-  center: [
-    40.7, -94.5
-  ],
-  zoom: 4
-});
+let map = L.map("mapid").setView([34.0522, -118.2437],14);
+  
+
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+/*
+L.circle([34.0522, -118.2437], {
+  radius: 100
+}).addTo(map);
+*/
+
+
+//Creating circleMarker() function  - to add a circle to the map 
+L.circleMarker([34.0522, -118.2437],{
+  radius : 300,
+  color:"black",
+  fillcolor:"#ffffa1"
+}).addTo(map);
+
 
 // Assign the variable map to the object L.map(). Instantiate the object with 'mapid'
 // The 'mapid' will reference the 'id' tag in the <div> element
 // The setView() method sets the view of the map with a geographical center, 
 //     where the first coordinate is the latitude and the second is longitude. Set the zoom level to "4" (0-18)
 
+
+/*
 // Create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+*/
+
+// Create the tile layer that will be the background of our map.- Dark 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
