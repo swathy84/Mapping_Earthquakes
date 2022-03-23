e4eea89b6c9ce38c6c4ff2bb5177d6d899cf0fea
swathy84 <93801581+swathy84@users.noreply.github.com>
2022-03-22 21:31:10 -0500

    Adding updated file to Mapping_GeoJSON_Points folder

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,10 +1,40 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-//Add a single line 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([37.6213, -122.3790], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+   }
  
+
+}).addTo(map);
+
+
 /*
 // Coordinates for each point to be used in the line.  for single line 
 let line = [
@@ -14,7 +44,7 @@ let line = [
 
 */
 
-
+/*
 // Coordinates for each point to be used in the polyline.
 let line = [
   [33.9416, -118.4085],
@@ -28,7 +58,7 @@ L.polyline(line, {
   color: "yellow"
 }).addTo(map);
 
-
+*/
 
 
 /*
@@ -82,7 +112,7 @@ L.circleMarker([34.0522, -118.2437],{
 
 
 // Create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
